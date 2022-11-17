6288a338cb537b2445310ef6c7e70720740e2b3b
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-17 01:15:53 -0500

    reading in port

--- server.js
+++ server.js
@@ -2,4 +2,9 @@ import express from 'express';
 import minimist from 'minimist'; 
 import { roll } from './lib/roll.js'
 
+const app = express(); 
+app.use(express.urlencoded({ extended: true}));
+const args = minimist(process.argv.slice(2));
 
+// read port 
+const port = args.port || 5000; 
