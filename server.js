a18ed14e6508fc10355f48594cae6ea11c57aa50
Nicholas Wong <69724812+nicwjh@users.noreply.github.com>
2022-11-17 01:21:39 -0500

    workshopping 3-5

--- server.js
+++ server.js
@@ -6,5 +6,25 @@ const app = express();
 app.use(express.urlencoded({ extended: true}));
 const args = minimist(process.argv.slice(2));
 
-// read port 
-const port = args.port || 5000; 
+// req #1 
+const port = args.port || 5000;
+
+// req #3
+app.get('/app/', (req, res) => {
+	res.send('200 OK');
+});
+
+// req #4
+app.get('/app/roll/', (req, res) => {
+	res.send(roll(6,2,1));
+});
+
+// req #5
+app.post('/app/roll/', (req, res) => {
+var sides = parseInt(req.body.sides);
+var dice = parseInt(req.body.dice);
+var rolls = parseInt(req.body.rolls);
+res.send(roll(sides,dice,rolls));
+});
+
+// req #6
